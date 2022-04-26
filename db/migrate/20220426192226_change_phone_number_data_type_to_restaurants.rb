ae4096071c9cdcc72906030af8cbd7879f323d66
Nuria Toledano <ia.andyouare@gmail.com>
2022-04-26 20:47:03 +0100

    Basic rails setup

---
+++ db/migrate/20220426192226_change_phone_number_data_type_to_restaurants.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumberDataTypeToRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    change_column :restaurants, :phone_number, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5 , category: "Italian"}
+# pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 4, category: "Pizza" }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+100.times do |i|
+  restaurant = Restaurant.create!({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: category.sample
+  })
+  puts "#{i} - #{restaurant.name} #{restaurant.phone_number} #{restaurant.category}"
+end
+
+
+puts "Finished!"
